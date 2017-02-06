c2ee233032e2c5dbac9edc0a31dd147fda416e31
rupesh-gatlewar <gatlewarrupeshkumar@gmail.com>
2017-02-06 16:04:05 +0530

    First commit

---
+++ src/test/java/com/test/scripts/ClassOneTest.java
@@ -0,0 +1,28 @@
+package com.test.scripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class ClassOneTest {
+	
+	WebDriver driver = null;
+	
+	@Test
+	public void TestOne()
+	{
+		System.setProperty("webdriver.marionette.firefox", "//resources//geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("http://www.amazon.in/");
+		Actions a = new Actions(driver);
+		a.moveToElement(driver.findElement(By.cssSelector("#nav-link-yourAccount"))).build().perform();
+		driver.findElement(By.xpath("//span[contains(.,'Sign in')]")).click();
+		driver.findElement(By.id("ap_email")).sendKeys("grk541@gmail.com");
+		driver.findElement(By.id("ap_email")).sendKeys("Amazon@1");
+		driver.findElement(By.id("signInSubmit")).click();
+		driver.quit();
+	}
+
+}
